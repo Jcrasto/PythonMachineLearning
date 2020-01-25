f6b0c379038cdfda363a3d0b1dd71ab33aef8df4
unknown <jcrasto@sbcglobal.net>
2020-01-25 14:24:03 -0800

    testing if changes on windows machine run on mac

---
+++ utils/perceptron.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+class Perceptron(object):
+    """Perceptron classifier
+	---Parameters---
+	eta : float - Learning rate (between 0.0 and 1.0)
+	n_iter : int - Iterates over the training dataset
+	---Attributes---
+	w_ :  1D array - weights after fitting
+	errors_ : list - number of misclassifications in every epoch
+	"""
+
+    def __init__(self, eta=0.01, n_iter=10):
+        self.eta = eta
+        self.n_iter = n_iter
+
+    def fit(self, X, _y):
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.errors_ = []
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, _y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
