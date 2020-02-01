51264bccb21a5297290de8f73c4cfc2835cc483c
unknown <jcrasto@sbcglobal.net>
2020-01-31 18:09:28 -0800

    adaline with updated weights with stochastic gradient descent

---
+++ utils/adaline_stochastic.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+
+class AdalineSGD(object):
+    """Adaptive Linear Neuron classifier
+	---Parameters---
+	eta : float - Learning rate (between 0.0 and 1.0)
+	n_iter : int - Iterates over the training dataset
+	---Attributes---
+	w_ :  1D array - weights after fitting
+	errors_ : list - number of misclassifications in every epoch
+	shuffle : bool - shuffling training data every epoch to prevent cycling
+	random_state : int - set random state for shuffling and initializing weights
+	"""
+
+    def __init__(self, eta=0.01, n_iter=10, shuffle=True, random_state=None):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.w_initialized = False
+        self.shuffle = shuffle
+        if random_state:
+            seed(random_state)
+
+    def fit(self, X, y):
+        """
+		X : m x n matrix where m is the number of samples and the n is the number of features (training data)
+		y : vector of target values 
+		"""
+        self._initialize_weights(X.shape[1])
+        self.cost_ = []
+        for i in range(self.n_iter):
+            if self.shuffle:
+                X, y = self._shuffle(X, y)
+            cost = []
+            for xi, target in zip(X, y):
+                cost.append(self._update_weights(xi, target))
+            avg_cost = sum(cost) / len(y)
+            self.cost_.append(avg_cost)
+        return self
+
+    def partial_fit(self, X, y):
+        """Fit training data without reinitializing the weights"""
+        if not self.w_initialized:
+            self._initialize_weights(X.shape[1])
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(X, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(X, y)
+        return self
+
+    def _shuffle(self, X, y):
+        r = np.random.permutation(len(y))
+        return X[r], y[r]
+
+    def _initialize_weights(self, m):
+        self.w_ = np.zeros(1 + m)
+        self.w_initialized = True
+
+    def _update_weights(self, xi, target):
+        output = self.net_input(xi)
+        error = target - output
+        self.w_[1:] += self.eta * xi.dot(error)
+        self.w_[0] += self.eta * error
+        cost = 0.5 * error ** 2
+        return cost
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, X):
+        """Compute linear activation"""
+        return self.net_input(X)
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.activation(X) >= 0.0, 1, -1)
