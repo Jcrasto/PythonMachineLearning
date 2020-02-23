3f81306ef374f7346af8f73dc3bd6bf2787fb5a6
joshcrasto <jcrasto@g.ucla.edu>
2020-02-22 18:41:04 -0600

    cleaning up

--- runscripts/decision_tree_classifier.py
+++ runscripts/decision_tree_classifier.py
@@ -4,52 +4,86 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 
 
-def one_hot_encode(df,encode_cols):
+def one_hot_encode(df, encode_cols):
     one_hot = pandas.get_dummies(df[encode_cols])
-    df = pandas.concat((df,one_hot), axis=1)
+    df = pandas.concat((df, one_hot), axis=1)
     col_names = df.columns.values
     df.reindex(columns=col_names)
-    return(df)
+    return df
 
-def create_decision_tree_model(df,numeric_cols):
+
+def create_decision_tree_model(df, numeric_cols):
     X = df[numeric_cols]
-    y = df['label']
-    train_X, val_X, train_y, val_y = train_test_split(X,y)
+    y = df["label"]
+    train_X, val_X, train_y, val_y = train_test_split(X, y)
 
     model = DecisionTreeClassifier(max_leaf_nodes=500, max_depth=100)
-    model.fit(train_X,train_y)
+    model.fit(train_X, train_y)
 
     predictions = model.predict(val_X)
     eval = pandas.DataFrame()
-    eval['actual'] = val_y
-    eval['predicted'] = predictions
-    eval['check'] = eval['actual'] == eval['predicted']
+    eval["actual"] = val_y
+    eval["predicted"] = predictions
+    eval["check"] = eval["actual"] == eval["predicted"]
     # return(model)
-    return(eval)
+    return eval
 
-def final_model_test(model,col_names,numeric_cols,encode_cols):
-    test_df = pandas.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test", skiprows=1, header=None,
-                         names=col_names, index_col=False)
-    test_df = one_hot_encode(test_df,encode_cols)
+
+def final_model_test(model, col_names, numeric_cols, encode_cols):
+    test_df = pandas.read_csv(
+        "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test",
+        skiprows=1,
+        header=None,
+        names=col_names,
+        index_col=False,
+    )
+    test_df = one_hot_encode(test_df, encode_cols)
     col_names = test_df.columns.values
     test_df.reindex(columns=col_names)
     X = test_df[numeric_cols]
-    y = test_df['label']
+    y = test_df["label"]
     predictions = model.predict(X)
     eval = pandas.DataFrame()
-    eval['actual'] = y
-    eval['predicted'] = predictions
-    eval['check'] = eval['actual'] == eval['predicted']
-    return(eval)
+    eval["actual"] = y
+    eval["predicted"] = predictions
+    eval["check"] = eval["actual"] == eval["predicted"]
+    return eval
+
 
 if __name__ == "__main__":
-    col_names = ["age", "workclass", "fnlwgt", "education", "education-num", "marital-status", "occupation",
-                 "relationship","race", "sex", "capital-gain", "capital-loss", "hours-per-week", "native-country", "label"]
-    df = pandas.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data", header=None,
-                         names=col_names, index_col=False)
+    col_names = [
+        "age",
+        "workclass",
+        "fnlwgt",
+        "education",
+        "education-num",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "capital-gain",
+        "capital-loss",
+        "hours-per-week",
+        "native-country",
+        "label",
+    ]
+    df = pandas.read_csv(
+        "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data",
+        header=None,
+        names=col_names,
+        index_col=False,
+    )
     numeric_cols = df.describe().columns.values
-    encode_cols = ['sex','race','workclass','marital-status','native-country','occupation']
-    df = one_hot_encode(df,encode_cols)
+    encode_cols = [
+        "sex",
+        "race",
+        "workclass",
+        "marital-status",
+        "native-country",
+        "occupation",
+    ]
+    df = one_hot_encode(df, encode_cols)
     numeric_cols = df.describe().columns.values
-    eval = create_decision_tree_model(df,numeric_cols)
-    print(str(eval['check'].sum() / eval.shape[0]) + "% predicted correctly")
+    eval = create_decision_tree_model(df, numeric_cols)
+    print(str(eval["check"].sum() / eval.shape[0]) + "% predicted correctly")
--- runscripts/random_forest_classifier.py
+++ runscripts/random_forest_classifier.py
@@ -7,58 +7,72 @@ import pandas
 import time
 from dask_ml.model_selection import train_test_split as dask_split
 
+
 def dask_run():
-    #df = dd.read_csv(...)
-    col_names = ["age", "workclass", "fnlwgt", "education", "education-num", "marital-status", "occupation",
-                 "relationship", "race", "sex", "capital-gain", "capital-loss", "hours-per-week", "native-country", "label"]
-    df = dd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data")
+    # df = dd.read_csv(...)
+    col_names = [
+        "age",
+        "workclass",
+        "fnlwgt",
+        "education",
+        "education-num",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "capital-gain",
+        "capital-loss",
+        "hours-per-week",
+        "native-country",
+        "label",
+    ]
+    df = dd.read_csv(
+        "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
+    )
     df = df.rename(columns=dict(zip(df.columns, col_names)))
     numeric_cols = df.describe().columns.values
     X = df[numeric_cols]
-    y = df['label']
+    y = df["label"]
     train_X, val_X, train_y, val_y = dask_split(X, y)
-    return(train_X, val_X, train_y, val_y) # This uses the single-machine scheduler by default
+    return (
+        train_X,
+        val_X,
+        train_y,
+        val_y,
+    )  # This uses the single-machine scheduler by default
+
 
 def pandas_run():
-    col_names = ["age", "workclass", "fnlwgt", "education", "education-num", "marital-status", "occupation",
-                 "relationship", "race", "sex", "capital-gain", "capital-loss", "hours-per-week", "native-country", "label"]
-    df = pandas.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data", header=None,
-                         names=col_names, index_col=False)
+    col_names = [
+        "age",
+        "workclass",
+        "fnlwgt",
+        "education",
+        "education-num",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "capital-gain",
+        "capital-loss",
+        "hours-per-week",
+        "native-country",
+        "label",
+    ]
+    df = pandas.read_csv(
+        "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data",
+        header=None,
+        names=col_names,
+        index_col=False,
+    )
     numeric_cols = df.describe().columns.values
     X = df[numeric_cols]
-    y = df['label']
+    y = df["label"]
     train_X, val_X, train_y, val_y = train_test_split(X, y)
-    return(train_X, val_X, train_y, val_y)
+    return (train_X, val_X, train_y, val_y)
 
-def pandas_rf(df):
-    start = time.time()
-    numeric_cols = df.describe().columns.values
-    X = df[numeric_cols]
-    y = df['label']
-    train_X, val_X, train_y, val_y = train_test_split(X, y)
-
-    model = RandomForestClassifier()
-    model.fit(train_X, train_y)
-
-    predictions = model.predict(val_X)
-    print(str(type(df)) + str(accuracy_score(val_y,predictions) * 100) + "% predicted correctly")
-    end = time.time()
-    print(str(type(df)) + " :took " + str(end - start))
-    return()
-
-def dask_rf(df):
-    numeric_cols = df.describe().columns.values
-    X = df[numeric_cols]
-    y = df['label']
-    train_X, val_X, train_y, val_y = dask_split(X, y)
-    model = RandomForestClassifier()
-    model.fit(train_X, train_y)
-
-    predictions = model.predict(val_X)
-    print(str(type(df)) + str(accuracy_score(val_y,predictions) * 100) + "% predicted correctly")
-    end = time.time()
-    # print(str(type(df)) + " :took " + str(end - start))
-    return()
 
 def create_rf(train_X, val_X, train_y, val_y):
     start = time.time()
@@ -66,12 +80,17 @@ def create_rf(train_X, val_X, train_y, val_y):
     model.fit(train_X, train_y)
 
     predictions = model.predict(val_X)
-    print(str(type(train_X)) + str(accuracy_score(val_y,predictions) * 100) + "% predicted correctly")
+    print(
+        str(type(train_X))
+        + str(accuracy_score(val_y, predictions) * 100)
+        + "% predicted correctly"
+    )
     end = time.time()
     print(str(type(train_X)) + " :took " + str(end - start))
-    return()
+    return ()
 
-if __name__== '__main__':
+
+if __name__ == "__main__":
     train_X, val_X, train_y, val_y = dask_run()
     create_rf(train_X, val_X, train_y, val_y)
     train_X, val_X, train_y, val_y = pandas_run()
