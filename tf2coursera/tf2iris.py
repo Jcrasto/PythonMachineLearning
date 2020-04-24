fec804523238c76d48d5670ea305ac25d1e67500
joshcrasto <jcrasto@g.ucla.edu>
2020-04-23 22:56:44 -0500

    nnet basics

---
+++ tf2coursera/tf2iris.py
@@ -0,0 +1,84 @@
+import pandas
+from sklearn.model_selection import train_test_split
+import math
+import numpy as np
+import tensorflow as tf
+import os
+import matplotlib.pyplot as plt
+
+tf.compat.v1.disable_eager_execution()
+
+iris = pandas.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',names=['sepal.length','sepal.width','petal.length','petal.width','species'])
+X = iris.loc[:, iris.columns != 'species']
+X =(X-X.min())/(X.max()-X.min())
+y = np.matrix(pandas.get_dummies(iris['species']))
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=1)
+
+# Parameters
+learning_rate = 0.01
+training_epochs = 100
+
+# Neural Network Parameters
+n_hidden_1 = 128  # 1st layer number of neurons
+n_hidden_2 = 256 # 1st layer number of neurons
+n_input = X_train.shape[1] # input shape (105, 4)
+n_classes = y_train.shape[1] # classes to predict
+
+# Inputs
+X = tf.compat.v1.placeholder("float", shape=[None, n_input])
+y = tf.compat.v1.placeholder("float", shape=[None, n_classes])
+# Dictionary of Weights and Biases
+weights = {
+  'h1': tf.Variable(tf.random.normal([n_input, n_hidden_1])),
+  'h2': tf.Variable(tf.random.normal([n_hidden_1, n_hidden_2])),
+  'out': tf.Variable(tf.random.normal([n_hidden_2, n_classes]))
+}
+biases = {
+  'b1': tf.Variable(tf.random.normal([n_hidden_1])),
+  'b2': tf.Variable(tf.random.normal([n_hidden_2])),
+  'out': tf.Variable(tf.random.normal([n_classes]))
+}
+
+# Model Forward Propagation step
+def forward_propagation(x):
+    # Hidden layer1
+    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    layer_1 = tf.nn.relu(layer_1)
+    
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    layer_2 = tf.nn.relu(layer_2)
+    # Output fully connected layer
+    out_layer = tf.matmul(layer_2, weights['out']) + biases['out'] 
+    return out_layer
+# Model Outputs
+yhat = forward_propagation(X)
+ypredict = tf.argmax(input=yhat, axis=1)
+
+# Backward propagation
+cost = tf.reduce_mean(input_tensor=tf.nn.softmax_cross_entropy_with_logits(labels=tf.stop_gradient(y), logits=yhat))
+optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate)
+#optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+train_op = optimizer.minimize(cost)
+
+# Initializing the variables
+init = tf.compat.v1.global_variables_initializer()
+from datetime import datetime
+startTime = datetime.now()
+with tf.compat.v1.Session() as sess:
+    sess.run(init)
+    
+    #writer.add_graph(sess.graph)
+    #EPOCHS
+    for epoch in range(training_epochs):
+        #Stochasting Gradient Descent
+        for i in range(len(X_train)):
+            summary = sess.run(train_op, feed_dict={X: X_train[i: i + 1], y: y_train[i: i + 1]})
+        
+        train_accuracy = np.mean(np.argmax(y_train, axis=1) == sess.run(ypredict, feed_dict={X: X_train, y: y_train}))
+        test_accuracy  = np.mean(np.argmax(y_test, axis=1) == sess.run(ypredict, feed_dict={X: X_test, y: y_test}))
+        if epoch % 5 == 0:         
+            print("Epoch = %d, train accuracy = %.2f%%, test accuracy = %.2f%%" % (epoch + 1, 100. * train_accuracy, 100. * test_accuracy))
+        #print("Epoch = %d, train accuracy = %.2f%%" % (epoch + 1, 100. * train_accuracy))
+    sess.close()
+print("Epoch = %d, train accuracy = %.2f%%, test accuracy = %.2f%%" % (epoch + 1, 100. * train_accuracy, 100. * test_accuracy))
+print("Time taken:", datetime.now() - startTime)
